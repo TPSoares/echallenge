b15d86e3cffabf3466d3c2a6766e66057e0e01e6
tpsoares <thiagopina.soares@gmail.com>
2024-09-22 19:43:50 -0300

    chore: setup project structure

---
+++ src/main/java/com/tpsoares/ebanxchallenge/EbanxChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.tpsoares.ebanxchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbanxChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbanxChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpsoares/ebanxchallenge/EbanxChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpsoares.ebanxchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbanxChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
